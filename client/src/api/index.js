a1e9254fa6c08f11c9f57985f1d2c3622e95d00c
Hung Nguyen <duchung2811nt@gmail.com>
2025-03-05 11:19:22 +0700

    attempt 3

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API= axios.create({baseURL:'http://localhost:5000'})
+const API= axios.create({baseURL:import.meta.mode==="development" ?'http://localhost:5000':"/"})
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
         req.headers.authorization=`Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
